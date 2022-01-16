7df5a100d375f441eb2af111e3a5fde1dadd0188
Vladislav Brezgun <nextydev@gmail.com>
2022-01-16 20:46:39 +0300

    added animation for home page

--- client/src/app/components/home-layout/home-header/home-header.component.ts
+++ client/src/app/components/home-layout/home-header/home-header.component.ts
@@ -1,9 +1,40 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'mbp-home-header',
     templateUrl: './home-header.component.html',
     styleUrls: ['./home-header.component.scss'],
+    animations: [
+        trigger('openWelcomeText', [
+            transition(':enter', [
+                style({
+                    opacity: '0',
+                    transform: 'translateY(160px)',
+                }),
+                animate(
+                    '.6s ease-in-out',
+                    style({
+                        opacity: '1',
+                        transform: 'translateY(0px)',
+                    }),
+                ),
+            ]),
+        ]),
+        trigger('openHeader', [
+            transition(':enter', [
+                style({
+                    opacity: '0',
+                }),
+                animate(
+                    '.8s ease-in',
+                    style({
+                        opacity: '1',
+                    }),
+                ),
+            ]),
+        ]),
+    ],
 })
 export class HomeHeaderComponent {
     constructor() {}
