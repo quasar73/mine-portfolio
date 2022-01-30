cbefd449c6f580161db0a8daed1487b2b99eb554
Vladislav Brezgun <nextydev@gmail.com>
2022-01-30 15:31:36 +0300

    removed test method from messages controller

--- src/controllers/messages.controller.ts
+++ src/controllers/messages.controller.ts
@@ -1,6 +1,5 @@
-import { ApiTags, ApiBody } from '@nestjs/swagger';
-import { Controller, Get } from '@nestjs/common';
-import { AuthDto } from 'src/shared/dto/auth.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Message } from 'src/database/entities/message.entity';
@@ -12,19 +11,4 @@ export class MessagesController {
         @InjectRepository(Message)
         private messageRepository: Repository<Message>,
     ) {}
-
-    @ApiBody({
-        type: AuthDto,
-    })
-    @Get('')
-    async test() {
-        const message = this.messageRepository.create({
-            message: '123',
-            discord: 'testdis',
-        });
-        const messageEntity = Message.create(message);
-        await messageEntity.save();
-
-        return messageEntity;
-    }
 }
